e30541d4270401de6375648d34860ea1a1b98640
yesenaer <45816977+yesenaer@users.noreply.github.com>
2023-12-01 10:48:01 +0100

    day 2 2023 prep

---
+++ events/twentythree/day_2.py
@@ -0,0 +1,14 @@
+def first_assignment():
+    with open('resources/day2_input.txt') as file:
+        pass
+    return 0
+
+
+def second_assignment():
+    with open('resources/day2_input.txt') as file:
+        pass
+    return 0
+
+
+print(f'answer to assignment 1 is: {first_assignment()}')
+print(f'answer to assignment 2 is: {second_assignment()}')
