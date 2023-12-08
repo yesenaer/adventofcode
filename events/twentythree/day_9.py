f7bd20ecdf0dc095bc306090221c4e18fe537d47
yesenaer <45816977+yesenaer@users.noreply.github.com>
2023-12-08 18:24:53 +0100

    day 9 2023 prep

---
+++ events/twentythree/day_9.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import pandas as pd
+
+RESOURCES_DIR = Path(__file__).parent / 'resources'
+
+
+def first_assignment():
+    result = 0
+    with open(RESOURCES_DIR / 'day9_input.txt') as file:
+        pass
+
+    return result
+
+
+def second_assignment():
+    result = 0
+    with open(RESOURCES_DIR / 'day9_input.txt') as file:
+        pass
+
+    return result
+
+
+print(f'answer to assignment 1 is: {first_assignment()}')
+print(f'answer to assignment 2 is: {second_assignment()}')
