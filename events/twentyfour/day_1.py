2631308a7dd7e1f3d9d0d521b5024fde22584bc9
jack.cornelisse <jack.cornelisse@nl.abnamro.com>
2024-12-02 15:18:18 +0100

    day one 2024

---
+++ events/twentyfour/day_1.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+RESOURCES = Path(__file__).parent / 'resources'
+
+
+def process_input():
+    first = []
+    second = []
+    with open(RESOURCES / 'day1_input.txt') as file:
+        for line in file:
+            splitted = line.split()
+            first.append(int(splitted[0]))
+            second.append(int(splitted[1]))
+    return first, second
+
+
+def first_assignment():
+    total_distance = 0
+    first, second = process_input()
+    first.sort()
+    second.sort()
+    
+    for i in range(len(first)):
+        if second[i] > first[i]:
+            diff = second[i] - first[i]
+        else: 
+            diff = first[i] - second[i]
+        total_distance += diff
+    return total_distance
+
+
+def second_assignment():
+    similarity_score = 0
+    first, second = process_input()
+    for i in range(len(first)):
+        count = second.count(first[i])
+        score = first[i] * count 
+        similarity_score += score
+    return similarity_score
+
+
+print(f'answer to assignment 1 is: {first_assignment()}')
+print(f'answer to assignment 2 is: {second_assignment()}')
