d3f87a95af20803cec89564a3e5054141490b7d3
jack.cornelisse <jack.cornelisse@nl.abnamro.com>
2024-12-12 10:37:23 +0100

    day3 part 1

---
+++ events/twentyfour/day_3.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import re
+
+RESOURCES = Path(__file__).parent / 'resources'
+
+
+
+def first_assignment():
+    multiplication_results = 0
+    with open(RESOURCES / 'day3_input.txt') as file:
+        for line in file:
+            hits = re.findall('m{1}u{1}l{1}\(\d{1,3}\,\d{1,3}\)', line)
+            for hit in hits:
+                digit = (hit.replace('mul(', '').replace(')', '')).split(',')
+                result = int(digit[0]) * int(digit[1])
+                multiplication_results += result             
+    return multiplication_results
+
+
+def second_assignment():
+    multiplication_results = 0
+    return multiplication_results
+
+
+print(f'answer to assignment 1 is: {first_assignment()}')  # 187825547
+print(f'answer to assignment 2 is: {second_assignment()}') 
