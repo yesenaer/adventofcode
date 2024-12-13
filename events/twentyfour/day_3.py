b1ff56c29f9f9c975bb60fbc1a1ea6fb857e49a9
jack.cornelisse <jack.cornelisse@nl.abnamro.com>
2024-12-13 12:37:14 +0100

    day3 part 2

--- events/twentyfour/day_3.py
+++ events/twentyfour/day_3.py
@@ -7,6 +7,8 @@ RESOURCES = Path(__file__).parent / 'resources'
 
 def first_assignment():
     multiplication_results = 0
+    
+    # process input and calculate results
     with open(RESOURCES / 'day3_input.txt') as file:
         for line in file:
             hits = re.findall('m{1}u{1}l{1}\(\d{1,3}\,\d{1,3}\)', line)
@@ -19,6 +21,34 @@ def first_assignment():
 
 def second_assignment():
     multiplication_results = 0
+    
+    # process input into one full command
+    full_command = "do()"  # starting condition
+    with open(RESOURCES / 'day3_input.txt') as file:
+        for line in file:
+            line = line.replace("\r", "").replace("\n", "")
+            full_command += line 
+
+    # have the condition change start a new line 
+    full_command = full_command.replace("don't()", "\ndon't()")
+    full_command = full_command.replace("do()", "\ndo()")
+
+    # conclude with writing updated commands to file
+    with open(RESOURCES / 'day3_output.txt', "w") as output:
+        output.write(full_command)
+    
+    # use updated commands to again process multiplications, as long as line is do()
+    with open(RESOURCES / 'day3_output.txt') as file:
+        for line in file:
+            if line.startswith("don't()"):
+                continue
+            else:
+                hits = re.findall('m{1}u{1}l{1}\(\d{1,3}\,\d{1,3}\)', line)
+                for hit in hits:
+                    digit = (hit.replace('mul(', '').replace(')', '')).split(',')
+                    result = int(digit[0]) * int(digit[1])
+                    multiplication_results += result  
+
     return multiplication_results
 
 
